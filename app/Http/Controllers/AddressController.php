e33366eb128766f492fea164b6ed3b2717690eae
nalivex <analiviat456@gmail.com>
2022-11-12 19:12:42 -0300

    adicionando comentarios

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -9,19 +9,20 @@ class AddressController extends Controller
     public function __invoke(Request $request)
     {
 
-        $cep = $request->cep;
+        $cep = $request->cep; //recendo valor passado no input por request
 
-        if((!empty($cep))){
+        if((!empty($cep))){    //verificando se valor passado está vazio para fazer a pesquisa na api de dados.
 
             $cep = preg_replace("/[^0-9]/", "", $cep);
 
-            $json = file_get_contents("http://viacep.com.br/ws/$cep/json/");
+            $json = file_get_contents("http://viacep.com.br/ws/$cep/json/"); //passando a api como valor json
 
-            $address= json_decode($json);
+            $address= json_decode($json); //retornando os dados como json
 
-            return view('address', compact('address'));
+            return view('address', compact('address')); //retornando a view enviando os dados necessários
         }
-        return view('address');
+
+        return view('address'); //caso o cep tenha vindo com valor vazio retorna a view sem dados
 
     }
 }
--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -53,7 +53,7 @@
     @endif
 
   </section>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script> --}}
 </body>
 
 </html>
